347b1e721efdf5930cc8d63631f51859974d8faf
mbrenman <mattbrenman@gmail.com>
2014-02-01 18:39:34 -0500

    sign up goes back to logiin

--- PoloApp/PoloApp/PoloViewController.m
+++ PoloApp/PoloApp/PoloViewController.m
@@ -53,25 +53,51 @@
     }
     
     
-    PFUser *user = [PFUser user];
-    user.username = @"julian";
-    user.password = @"a";
-    user.email = @"email@example.com";
+//    PFUser *user = [PFUser user];
+//    user.username = @"julian";
+//    user.password = @"a";
+//    user.email = @"email@example.com";
+//    
+//    // other fields can be set just like with PFObject
+//    user[@"lat"] = [NSString stringWithFormat:@"%f", _locationManager.location.coordinate.latitude];
+//    user[@"long"] = [NSString stringWithFormat:@"%f", _locationManager.location.coordinate.longitude];
+//    
+//    
+//    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+//        if (!error) {
+//            // Hooray! Let them use the app now.
+//        } else {
+//            NSString *errorString = [error userInfo][@"error"];
+//            NSLog(errorString);
+//            // Show the errorString somewhere and let the user try again.
+//        }
+//    }];
+}
+
+// Sent to the delegate to determine whether the log in request should be submitted to the server.
+- (BOOL)logInViewController:(PFLogInViewController *)logInController shouldBeginLogInWithUsername:(NSString *)username password:(NSString *)password {
+    // Check if both fields are completed
+    if (username && password && username.length != 0 && password.length != 0) {
+        return YES; // Begin login process
+    }
     
-    // other fields can be set just like with PFObject
-    user[@"lat"] = [NSString stringWithFormat:@"%f", _locationManager.location.coordinate.latitude];
-    user[@"long"] = [NSString stringWithFormat:@"%f", _locationManager.location.coordinate.longitude];
-    
-    
-    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-        if (!error) {
-            // Hooray! Let them use the app now.
-        } else {
-            NSString *errorString = [error userInfo][@"error"];
-            NSLog(errorString);
-            // Show the errorString somewhere and let the user try again.
-        }
-    }];
+    [[[UIAlertView alloc] initWithTitle:@"Missing Information"
+                                message:@"Make sure you fill out all of the information!"
+                               delegate:nil
+                      cancelButtonTitle:@"ok"
+                      otherButtonTitles:nil] show];
+    return NO; // Interrupt login process
+}
+
+// Sent to the delegate when a PFUser is logged in.
+- (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user {
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+// Sent to the delegate when a PFUser is signed up.
+- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
+    //Go back to login screen
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 - (void)didReceiveMemoryWarning

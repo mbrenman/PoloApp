5b24849dcfbeee91cd5e1cafe7175da283571e87
mbrenman <mattbrenman@gmail.com>
2014-02-01 17:49:32 -0500

    Added additional field to signups

--- PoloApp/PoloApp/PoloViewController.m
+++ PoloApp/PoloApp/PoloViewController.m
@@ -7,10 +7,11 @@
 //
 
 #import "PoloViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
 
 @interface PoloViewController ()
-
+    @property (nonatomic) CLLocationManager *locationManager;
 @end
 
 @implementation PoloViewController
@@ -19,26 +20,58 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.distanceFilter = kCLDistanceFilterNone;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [_locationManager startUpdatingLocation];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    [PFUser logOut];
     if (![PFUser currentUser]) { // No user logged in
         // Create the log in view controller
         PFLogInViewController *logInViewController = [[PFLogInViewController alloc] init];
         [logInViewController setDelegate:self]; // Set ourselves as the delegate
         
+        logInViewController.fields = PFLogInFieldsUsernameAndPassword | PFLogInFieldsLogInButton | PFLogInFieldsSignUpButton | PFLogInFieldsPasswordForgotten;
+        
         // Create the sign up view controller
         PFSignUpViewController *signUpViewController = [[PFSignUpViewController alloc] init];
         [signUpViewController setDelegate:self]; // Set ourselves as the delegate
+
+        [signUpViewController setFields:PFSignUpFieldsDefault | PFSignUpFieldsAdditional];
+        [[[signUpViewController signUpView] additionalField] setPlaceholder:@"Phone Number"];
         
         // Assign our sign up controller to be displayed from the login controller
         [logInViewController setSignUpController:signUpViewController];
+
         
         // Present the log in view controller
         [self presentViewController:logInViewController animated:YES completion:NULL];
     }
+    
+    
+    PFUser *user = [PFUser user];
+    user.username = @"julian";
+    user.password = @"a";
+    user.email = @"email@example.com";
+    
+    // other fields can be set just like with PFObject
+    user[@"lat"] = [NSString stringWithFormat:@"%f", _locationManager.location.coordinate.latitude];
+    user[@"long"] = [NSString stringWithFormat:@"%f", _locationManager.location.coordinate.longitude];
+    
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            // Hooray! Let them use the app now.
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            NSLog(errorString);
+            // Show the errorString somewhere and let the user try again.
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning

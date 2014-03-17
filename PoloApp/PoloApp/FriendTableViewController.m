93535e4923c2ae4e2c6331948d75e259a808caa4
mbrenman <mattbrenman@gmail.com>
2014-03-17 00:10:50 -0400

    friend table needed to reload data after push return

--- PoloApp/PoloApp/FriendTableViewController.m
+++ PoloApp/PoloApp/FriendTableViewController.m
@@ -102,6 +102,11 @@
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.tableView reloadData];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];

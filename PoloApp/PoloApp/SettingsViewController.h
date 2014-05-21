23564671ef9456dcc26c25fea414eea6fe828d53
mbrenman <mattbrenman@gmail.com>
2014-05-20 21:53:43 -0400

    added settings page view/viewcontroller

---
+++ PoloApp/PoloApp/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  PoloApp
+//
+//  Created by Matt Brenman on 5/20/14.
+//  Copyright (c) 2014 mbrenman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ PoloApp/PoloApp/SettingsViewController.m
@@ -0,0 +1,55 @@
+//
+//  SettingsViewController.m
+//  PoloApp
+//
+//  Created by Matt Brenman on 5/20/14.
+//  Copyright (c) 2014 mbrenman. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    NSLog(@"SETTINGS!");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

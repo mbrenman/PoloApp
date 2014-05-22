32c6b72a20622baa28a9b9098005ddee9036231b
Julian Locke <julian.r.locke@gmail.com>
2014-05-22 17:59:29 -0400

    grouped files and added PoloLocationManager objec

---
+++ PoloApp/PoloApp/PoloLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  PoloLocationManager.h
+//  PoloApp
+//
+//  Created by Susanne Heincke on 5/22/14.
+//  Copyright (c) 2014 mbrenman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PoloLocationManager : NSObject
+
+@end
---
+++ PoloApp/PoloApp/PoloLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  PoloLocationManager.m
+//  PoloApp
+//
+//  Created by Susanne Heincke on 5/22/14.
+//  Copyright (c) 2014 mbrenman. All rights reserved.
+//
+
+#import "PoloLocationManager.h"
+
+@implementation PoloLocationManager
+
+@end

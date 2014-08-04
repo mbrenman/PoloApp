c8ac404319291493fa30c6b06daef80ab7652f04
julianlocke <julian.r.locke@gmail.com>
2014-08-04 13:10:44 -0400

    removed vestigal testing code

--- PoloApp/PoloApp/PoloAppDelegate.m
+++ PoloApp/PoloApp/PoloAppDelegate.m
@@ -44,10 +44,7 @@
      UIRemoteNotificationTypeBadge |
      UIRemoteNotificationTypeAlert |
      UIRemoteNotificationTypeSound];
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self application:application didReceiveRemoteNotification:nil  ];
-    });
+
     
     return YES;
 }
